cddcdebe6a4f8223d76c8f17e308083061bafc4e
aymanshawky <aymanshawky2009@gmail.com>
2019-12-15 01:03:15 +0200

    Updates

---
+++ precache-manifest.3a9f1c9d49742f12c6d944dce1b6791a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367764cd9fcd86d4cb61e62d41743273",
+    "url": "/EditableTable-React/index.html"
+  },
+  {
+    "revision": "2866bf37969b250e8ff3",
+    "url": "/EditableTable-React/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "529085188ee23203442e",
+    "url": "/EditableTable-React/static/css/main.cf19aa85.chunk.css"
+  },
+  {
+    "revision": "2866bf37969b250e8ff3",
+    "url": "/EditableTable-React/static/js/2.b311b001.chunk.js"
+  },
+  {
+    "revision": "886eee7b5b13f54e24f2fe91714d56e3",
+    "url": "/EditableTable-React/static/js/2.b311b001.chunk.js.LICENSE"
+  },
+  {
+    "revision": "529085188ee23203442e",
+    "url": "/EditableTable-React/static/js/main.76056fe2.chunk.js"
+  },
+  {
+    "revision": "2b38e95150638f6759fa",
+    "url": "/EditableTable-React/static/js/runtime-main.4b6e435c.js"
+  }
+]);
